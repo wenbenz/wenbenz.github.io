05a5bfd82444f0caa0522b9e0e6d476dd968d1db
wenbenz <ben@wenbinzhao.com>
2017-12-17 01:32:30 -0500

    formatted gulpfile and added TS

--- gulpfile.js
+++ gulpfile.js
@@ -2,26 +2,53 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 var minifyCSS = require('gulp-csso');
 
+/* PATHS */
+const html = 'src/*.html';
+const images = 'src/img/*';
+const favicon = 'src/favicon.ico';
+const stylesheets = 'src/css/*.{css,sass,scss}';
+const fonts = 'src/fonts/*';
+const files = 'src/files/*';
+
 gulp.task('HTML', function () {
-    return gulp.src('src/*.html')
+    return gulp.src(html)
         .pipe(gulp.dest('build'))
 });
 
 gulp.task('images', function () {
-    return gulp.src('src/img/*')
+    return gulp.src(images)
         .pipe(gulp.dest('build/img'))
 });
 
+gulp.task('favicon', function () {
+    return gulp.src(favicon)
+        .pipe(gulp.dest('build'))
+})
+
 gulp.task('Sass/SCSS', function () {
-    return gulp.src('src/css/*.{sass,scss}')
+    return gulp.src(stylesheets)
         .pipe(sass())
         .pipe(minifyCSS())
         .pipe(gulp.dest('build/css'))
 });
 
 gulp.task('fonts', function () {
-    return gulp.src('src/fonts/*')
+    return gulp.src(fonts)
         .pipe(gulp.dest('build/fonts'))
 });
 
-gulp.task('default', ['HTML', 'images', 'Sass/SCSS']);
+gulp.task('files', function () {
+    return gulp.src(files)
+        .pipe(gulp.dest('build/files'))
+})
+
+gulp.task('watch', function () {
+    gulp.watch(html, ['HTML'])
+    gulp.watch(stylesheets, ['Sass/SCSS'])
+    gulp.watch(images, ['images'])
+    gulp.watch(fonts, ['fonts'])
+    gulp.watch(favicon, ['favicon'])
+    gulp.watch(files, ['files'])
+});
+
+gulp.task('default', ['HTML', 'images', 'favicon', 'Sass/SCSS', 'fonts']);
