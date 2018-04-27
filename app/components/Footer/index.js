e2f3151f4402a5ba96678ef216df752003bff4a2
wenbenz <ben@wenbinzhao.com>
2018-04-27 14:22:22 -0400

    removed unnecessary component

--- app/components/Footer/index.js
+++ app/components/Footer/index.js
@@ -1,11 +1,20 @@
 const React = require('react');
-const { IconBar } = require('../IconBar');
 
 export class Footer extends React.Component {
     render() {
         return (
             <footer>
-                <IconBar />
+                <div className="iconLinks">
+                    <a href="https://github.com/wenbenz">
+                        <i className="fab fa-github fa-3x" aria-hidden="true"></i>
+                    </a>
+                    <a href="https://ca.linkedin.com/in/wenbinzhao">
+                        <i className="fab fa-linkedin fa-3x" aria-hidden="true"></i>
+                    </a>
+                    <a href="files/resume.pdf">
+                        <i className="fas fa-file fa-3x" aria-hidden="true"></i>
+                    </a>
+                </div>
                 <hr />
                 <div>
                     © Wenbin Zhao 2018, All rights reserved.
